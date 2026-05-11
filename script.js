16a94072b806b841f8e022f1dd2cc7091e7be34a
ALI <alialbuni337@gmail.com>
2026-05-11 11:23:53 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -21,3 +21,20 @@
       document.getElementById("hasilkeliling").innerHTML=`rumus: 4 x${sisi}<br> hasil;
       ${hasil}`;
     }
+
+
+
+    
+    function hitungluas() {
+      let lebar= document.getElementById("llebar").value;
+      let panjang= document.getElementById("lpanjang").value;
+      if (panjang === "" || lebar||panjang<=0 || lebar <=0){
+        alert("masukan nilai sisi yang valid");
+        return;
+      }
+      let hasil= panjang * lebar;
+      document.getElementById("hasil").innerHTML=`rumus: $(panjang) x${lebar}<br> hasil;
+      ${hasil}`;
+    }
+
+  
